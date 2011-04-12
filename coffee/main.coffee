9214f26f6930b9b607b568041a6533f6567ac6f1
Aanand Prasad <aanand.prasad@gmail.com>
2011-04-12 20:21:28 +0100

    Load different CSS for iPhone

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,5 +1,12 @@
+Mobile = navigator.userAgent.match /iPhone/
 
+loadCSS = (path) ->
+  $('head').append("<link rel='stylesheet' type='text/css' href='#{path}?#{CacheBuster}'/>")
 
+if Mobile
+  loadCSS('/css/mobile.css')
+else
+  loadCSS('/css/main.css')
 
 App = new AppView
 $(document.body).append(App.render().el)
--- public/coffee/main.js
+++ public/coffee/main.js
@@ -1,3 +1,12 @@
-var App;
+var App, Mobile, loadCSS;
+Mobile = navigator.userAgent.match(/iPhone/);
+loadCSS = function(path) {
+  return $('head').append("<link rel='stylesheet' type='text/css' href='" + path + "?" + CacheBuster + "'/>");
+};
+if (Mobile) {
+  loadCSS('/css/mobile.css');
+} else {
+  loadCSS('/css/main.css');
+}
 App = new AppView;
 $(document.body).append(App.render().el);
