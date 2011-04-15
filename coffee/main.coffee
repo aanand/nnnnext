158564c2a279fce429373f080489906d27ae5859
Aanand Prasad <aanand.prasad@gmail.com>
2011-04-15 14:16:27 +0100

    Move CSS loading into inline script tag in <head>

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,19 +1,11 @@
-Mobile = navigator.userAgent.match /iPhone/
-
-loadCSS = (path) ->
-  $('head').append("<link rel='stylesheet' type='text/css' href='#{path}?#{CacheBuster}'/>")
-
 if Mobile
   UI = Touch
-
-  loadCSS('/css/mobile.css')
 
   $('head')
     .append('<meta name="apple-mobile-web-app-capable" content="yes">')
     .append('<meta name="viewport" content="width=640,user-scalable=no">')
 else
   UI = Desktop
-  loadCSS('/css/main.css')
 
 App = new UI.AppView
 App.appendTo(document.body)
--- public/coffee/main.js
+++ public/coffee/main.js
@@ -1,15 +1,9 @@
-var App, Mobile, UI, loadCSS;
-Mobile = navigator.userAgent.match(/iPhone/);
-loadCSS = function(path) {
-  return $('head').append("<link rel='stylesheet' type='text/css' href='" + path + "?" + CacheBuster + "'/>");
-};
+var App, UI;
 if (Mobile) {
   UI = Touch;
-  loadCSS('/css/mobile.css');
   $('head').append('<meta name="apple-mobile-web-app-capable" content="yes">').append('<meta name="viewport" content="width=640,user-scalable=no">');
 } else {
   UI = Desktop;
-  loadCSS('/css/main.css');
 }
 App = new UI.AppView;
 App.appendTo(document.body);
