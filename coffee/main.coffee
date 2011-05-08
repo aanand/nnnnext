f8a9d25a7c87430659d1cd4032b31f152bbd72a3
Aanand Prasad <aanand.prasad@gmail.com>
2011-05-09 00:03:22 +0100

    Touch icon
    Closes #28

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -5,6 +5,7 @@ if Mobile
     .append('<meta name="apple-mobile-web-app-capable" content="yes">')
     .append('<meta name="viewport" content="width=640,user-scalable=no">')
     .append('<link rel="apple-touch-startup-image" href="/img/startup.png">')
+    .append('<link rel="apple-touch-icon" href="/img/icon.png">')
 else
   UI = Desktop
 
--- public/coffee/main.js
+++ public/coffee/main.js
@@ -1,7 +1,7 @@
 var App, UI;
 if (Mobile) {
   UI = Touch;
-  $('head').append('<meta name="apple-mobile-web-app-capable" content="yes">').append('<meta name="viewport" content="width=640,user-scalable=no">').append('<link rel="apple-touch-startup-image" href="/img/startup.png">');
+  $('head').append('<meta name="apple-mobile-web-app-capable" content="yes">').append('<meta name="viewport" content="width=640,user-scalable=no">').append('<link rel="apple-touch-startup-image" href="/img/startup.png">').append('<link rel="apple-touch-icon" href="/img/icon.png">');
 } else {
   UI = Desktop;
 }
