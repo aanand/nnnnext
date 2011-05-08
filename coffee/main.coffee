7dc267f1843febd0807d168ba3b5489c3c929205
Aanand Prasad <aanand.prasad@gmail.com>
2011-05-08 23:45:26 +0100

    Startup screen
    Closes #29

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -4,6 +4,7 @@ if Mobile
   $('head')
     .append('<meta name="apple-mobile-web-app-capable" content="yes">')
     .append('<meta name="viewport" content="width=640,user-scalable=no">')
+    .append('<link rel="apple-touch-startup-image" href="/img/startup.png">')
 else
   UI = Desktop
 
--- public/coffee/main.js
+++ public/coffee/main.js
@@ -1,7 +1,7 @@
 var App, UI;
 if (Mobile) {
   UI = Touch;
-  $('head').append('<meta name="apple-mobile-web-app-capable" content="yes">').append('<meta name="viewport" content="width=640,user-scalable=no">');
+  $('head').append('<meta name="apple-mobile-web-app-capable" content="yes">').append('<meta name="viewport" content="width=640,user-scalable=no">').append('<link rel="apple-touch-startup-image" href="/img/startup.png">');
 } else {
   UI = Desktop;
 }
