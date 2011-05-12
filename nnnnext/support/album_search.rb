046642c6b444a64270314096468e8ed44e4ddcc3
Aanand Prasad <aanand.prasad@gmail.com>
2011-05-12 16:35:28 +0100

    Improve search relevance for artists with remix and cover albums

--- nnnnext/support/album_search.rb
+++ nnnnext/support/album_search.rb
@@ -8,8 +8,27 @@ module AlbumSearch
 
   def self.search(q)
     q = q.gsub('"', '')
-    words = q.split + ["\"#{q}\""]
-    query = (words + words.map { |w| "artist:#{w}" }).join(" ")
+    words = q.split
+
+    terms = []
+
+    if words.length > 1
+      # search for individual words in title & artist
+      terms += words
+      terms += words.map { |w| "artist:#{w}" }
+    end
+
+    terms << "\"#{q}\""          # boost exact match
+    terms << "artist:\"#{q}\"^2" # boost exact artist match extra hard
+
+    # boost official albums over compilations, remixes etc
+    terms += ['type:album', 'status:official']
+
+    query = terms.join(" ")
+
+    # puts
+    # puts query
+    # puts
 
     response = get("/ws/1/release/", query: {query: query})
 
@@ -26,6 +45,8 @@ module AlbumSearch
       id     = release["id"]
       title  = release["title"]
       artist = release["artist"]["name"]
+
+      # p [artist, title, release["type"]]
 
       unless albums.any? { |a| a[:title] == title and a[:artist] == artist }
         albums << { id: id, title: title, artist: artist }
