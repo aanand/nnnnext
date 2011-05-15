89517d0a280cd9e7fe96fbbe1ef82d53e7e67b29
Aanand Prasad <aanand.prasad@gmail.com>
2011-05-15 18:05:40 +0100

    Remove header button CSS

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -3,7 +3,7 @@ if Platform == "mobile"
 
   $('head')
     .append('<meta name="apple-mobile-web-app-capable" content="yes">')
-    .append('<meta name="viewport" content="width=640,user-scalable=no">')
+    .append('<meta name="viewport" content="width=device-width,user-scalable=no">')
     .append('<link rel="apple-touch-startup-image" href="/img/startup.png">')
     .append('<link rel="apple-touch-icon" href="/img/icon.png">')
 else
--- public/coffee/main.js
+++ public/coffee/main.js
@@ -1,7 +1,7 @@
 var App, UI;
 if (Platform === "mobile") {
   UI = Touch;
-  $('head').append('<meta name="apple-mobile-web-app-capable" content="yes">').append('<meta name="viewport" content="width=640,user-scalable=no">').append('<link rel="apple-touch-startup-image" href="/img/startup.png">').append('<link rel="apple-touch-icon" href="/img/icon.png">');
+  $('head').append('<meta name="apple-mobile-web-app-capable" content="yes">').append('<meta name="viewport" content="width=device-width,user-scalable=no">').append('<link rel="apple-touch-startup-image" href="/img/startup.png">').append('<link rel="apple-touch-icon" href="/img/icon.png">');
 } else {
   UI = Desktop;
 }
